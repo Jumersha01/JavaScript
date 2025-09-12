2eb3af7aa5a97d0ad2fc988b742f9013e9473940
Jumersha01 <www.junaid555fakir@gmail.com>
2025-09-12 19:38:44 +0530

    Adding all the file of day 4

---
+++ Day-4/script.js
@@ -0,0 +1,24 @@
+let str ="    Junaid shah   "
+let str2 = str.trim()
+ console.log(str2)
+
+ let a = 10;
+ let x = ++a + a++;
+ console.log(x)//22
+ x =10,b=5
+ x = ++a + b-- + --a + ++b;//11 + 5 + 10 + 5
+ console.log(x)//55
+
+
+
+ let num =+ prompt("Enter the num")
+ let result = (num%2==0) ? "Even":"Odd";
+ console.log(`${num } is ${result}`);
+
+  let l  = 10;
+     let m = 20;
+     let n = l??m;
+     console.log(n);//10;
+
+
+ 
