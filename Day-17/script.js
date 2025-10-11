3fc8e6ab15d31af389393945aed48ce68d014177
Jumersha01 <www.junaid555fakir@gmail.com>
2025-10-10 23:05:57 -1200

    Adding class work of javascript on Asynchronous Programming

---
+++ Day-17/script.js
@@ -0,0 +1,36 @@
+let x =20000;
+let p  = new Promise((resolve,reject)=>{
+   if(x<30000){
+    resolve("You can take the money")
+   }
+   else{
+    reject("No Money");
+   }
+})
+
+p.then((data)=>{
+    console.log(data)
+}).catch(()=>{
+    console.log("error");
+})
+let imgs = document.body.querySelector('.im')
+console.log(imgs)
+
+
+document.body.addEventListener("Keydown",(e)=>{
+   if(e.key=="Enter"){
+     let dog = document.querySelector(".inp").value
+    console.log(dog)
+    async function a() {
+    let a = await fetch(`https://dog.ceo/api/${dog}/image/random`);
+    let b = await a.json()
+let s = b.message;
+console.log(s)
+imgs.src=s
+
+
+}
+a()
+   }
+
+})
